8db207ecd0ae22b10875121a3c751e5a282bdf0f
Alexlopez52 <alexlopeztbj@gmail.com>
2020-04-17 10:53:11 +0200

    minimo1 borrado archivos innecesarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {subject} from './models/subject';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http'; //poner para usar http
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
-import { SubjectComponent } from './subject/subject.component';
+
 import {FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { InstitucionComponent } from './institucion/institucion.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent,
-    SubjectComponent,
     InstitucionComponent
   ],
   imports: [
